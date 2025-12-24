06014afe3f3ce479d549b0e0fb3e7114b4e95ee8
Hx <hxlee02@gmail.com>
2025-12-24 19:13:43 +0800

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { useUser } from "@/stores/useUser";
+import GuestBooking from "@/components/custom/GuestBooking";
 
 export default function Home() {
-  return (
-    <div>
-    </div>
-  );
+	const { isLoggedIn } = useUser();
+	const [hydrated, setHydrated] = useState(false);
+
+	useEffect(() => {
+		setHydrated(true);
+	}, []);
+
+	if (!hydrated) return null;
+
+	if (isLoggedIn()) {
+		window.location.href = "/dashboard";
+	} else {
+		window.location.href = "/login";
+	}
+
+	return (
+		<div></div>
+	);
 }
